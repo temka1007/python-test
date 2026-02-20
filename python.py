699801c0bfcd921188178f185fd05273be287a6b
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2026-02-20 20:42:23 +0900

    clearing the field

--- python.py
+++
@@ -1,5 +0,0 @@
-from sklearn import metrics
-
-labels_true = [0, 0, 0, 1, 1, 1]
-labels_pred = [0, 0, 1, 1, 2, 2]
-metrics.rand_score(labels_true, labels_pred)
--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "this can't be real" << endl;
-    return 0;
-}
